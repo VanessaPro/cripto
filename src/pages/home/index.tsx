47fba0354912b330b2701aeaa49e052fe93d9723
Vanessa Santos <114835013+VanessaPro@users.noreply.github.com>
2023-07-18 15:20:39 -0300

    fix

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,69 @@
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import styles from './home.module.css'
 import {BiSearch} from 'react-icons/bi'
+import { Link } from 'react-router-dom'
 
+
+//https://coinlib.io/api/v1/coinlist?key=bb37b73cb5fc8e46&
+
+//Sua chave de API: bb37b73cb5fc8e46
+
+interface CoinsProps{
+    name:string;
+    delta_24h:string;
+    price:string;
+    symbol:string;
+    volume_24h:string
+    market_cap:string;
+    formatedPrice:string;
+    formatedMarket:string;
+
+}
+
+interface DataProps{
+    coins: CoinsProps[];
+}
 
 export function Home (){
+    const [coins, setCoins] =useState<CoinsProps[]>([])
+   
+    useEffect(() => {
+   
+        function getData() {
+     
+          fetch('https://sujeitoprogramador.com/api-cripto/?key=bb37b73cb5fc8e46&pref=BRL')
+           .then(response => response.json()) 
+           .then((data: DataProps) => {
+              let coinsData = data.coins.slice(0, 15);
+
+              let price = Intl.NumberFormat("pt-BR",{
+                style:"currency",
+                currency:"BRL"
+              })
+
+              const formatresult = coinsData.map((item) =>{
+                const formated ={
+                    ...item,
+                    formatedPrice:price.format(Number(item.price)),
+                    formatedMarket:price.format(Number(item.market_cap)),
+                }
+
+                return formated;
+              })
+               
+             
+              setCoins(formatresult);
+           })
+           
+          
+        },
+    
+        getData();
+
+
+
+    }, [])
+
     return(
         
         <main className={styles.container}>
